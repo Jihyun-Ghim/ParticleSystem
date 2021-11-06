e741845818ebc10859b5768301d453bb6c00695b
Jihyun-Ghim <84126943+Jihyun-Ghim@users.noreply.github.com>
2021-11-06 20:03:48 +0900

    start

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let particle;
+
+function setup() {
+  createCanvas(400, 400);
+  particle = new Particle(200,200);
+}
+
+function draw() {
+  background(0);
+
+  let gravity = createVector(0, 0.2);
+  particle.applyForce(gravity);
+
+  particle.update();
+  particle.edges();
+  particle.show();
+}
